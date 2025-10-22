fb1afd209377b427b48438b7003810c80e313b58
SRI15012003 <111505051+SRI15012003@users.noreply.github.com>
2025-10-22 17:54:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function openPDF(fileName) {
-  window.location.href = `viewer.html?file=${fileName}`;
+function openFlipbook(url) {
+  // Pass the URL as a query parameter to viewer.html
+  window.location.href = "viewer.html?url=" + encodeURIComponent(url);
 }
+
